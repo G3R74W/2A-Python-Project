386cfadf8a17f69df05bf3bcd587e95ddca337e1
tobias <t.wendl28@gmail.com>
2022-04-13 17:03:03 +0200

    test n°3

--- main.py
+++ main.py
@@ -3,22 +3,19 @@
 import pygame
 from pygame import*
 
-
-
-
-
-
 ###################################################################
 ## MENU
 ###################################################################
 
 def menu():
+    """Menu de la borne d'arcade"""
     print("menu")
     pygame.init()
     window = pygame.display.set_mode((800,800))
     pygame.display.set_caption("Projet 2A") #a changer
-
+    
     run = True
+
     while run:
 
         for event in pygame.event.get():
@@ -26,13 +23,4 @@ def menu():
                 run = False
     pygame.quit()
 
-def main():
-        print("main")
-        menu()
-
-if __name__ == "__main__":
-    main()
-
-print('hello')
-
-##changement test
+menu()
