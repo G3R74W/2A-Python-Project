b8bf67392e2936adadac5e53ed63807a0858a769
tobias <t.wendl28@gmail.com>
2022-04-13 16:53:31 +0200

    Changes by guerq

--- main.py
+++ main.py
@@ -2,21 +2,35 @@
 
 import pygame
 from pygame import*
-def main () :
-    print("main")
 
 
+
+
+
+
+###################################################################
+## MENU
+###################################################################
+
+def menu():
+    print("menu")
+    pygame.init()
+    window = pygame.display.set_mode((800,800))
+    pygame.display.set_caption("Projet 2A") #a changer
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+def main():
+        print("main")
+        menu()
 
 if __name__ == "__main__":
     main()
-pygame.init()
-window = pygame.display.set_mode((800,800))
-pygame.display.set_caption("Projet 2A") #a changer
 
-run = True
-while run:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-pygame.quit()
+print('hello')
