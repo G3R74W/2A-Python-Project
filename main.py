d3032b88bec182fd373ed22ecf80da4d3167f90d
tobias <t.wendl28@gmail.com>
2022-05-04 20:10:13 +0200

    changement menu

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
 #-*- coding: UTF-8 -*-
 #@tobias_Wendl
 
-import pygame
+import pygame, sys, collision
 from pygame import*
-import collision
-from collision import*
 
+class Button:
+	def __init__(self,text,width,height,pos,elevation):
 
+		gui_font = pygame.font.Font(None, 30)
+		#Core attributes
+		self.pressed = False
+		self.elevation = elevation
+		self.dynamic_elecation = elevation
+		self.original_y_pos = pos[1]
 
-##############
-## --MENU-- ##
-##############
+		# top rectangle
+		self.top_rect = pygame.Rect(pos,(width,height))
+		self.top_color = '#475F77'
+
+		# bottom rectangle
+		self.bottom_rect = pygame.Rect(pos,(width,height))
+		self.bottom_color = '#354B5E'
+		#text
+		self.text_surf = gui_font.render(text,True,'#FFFFFF')
+		self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+
+	def draw(self):
+		# elevation logic
+		self.top_rect.y = self.original_y_pos - self.dynamic_elecation
+		self.text_rect.center = self.top_rect.center
+
+		self.bottom_rect.midtop = self.top_rect.midtop
+		self.bottom_rect.height = self.top_rect.height + self.dynamic_elecation
+
+		pygame.draw.rect(window,self.bottom_color, self.bottom_rect,border_radius = 12)
+		pygame.draw.rect(window,self.top_color, self.top_rect,border_radius = 12)
+		window.blit(self.text_surf, self.text_rect)
+		self.check_click()
+
+	def check_click(self):
+		mouse_pos = pygame.mouse.get_pos()
+		if self.top_rect.collidepoint(mouse_pos):
+			self.top_color = '#D74B4B'
+			if pygame.mouse.get_pressed()[0]:
+				self.dynamic_elecation = 0
+				self.pressed = True
+			else:
+				self.dynamic_elecation = self.elevation
+				if self.pressed == True:
+					print('click')
+					self.pressed = False
+		else:
+			self.dynamic_elecation = self.elevation
+			self.top_color = '#475F77'
 
 def menu_buttons():
     """menu buttons"""
@@ -72,16 +114,19 @@ white = (255, 255, 255)
 line_color = (179, 254, 255)
 background_color = (191, 255, 107)
 
+button1 = Button('The Square Game', 200, 40, (20, 250), 5)
 
+clock = pygame.time.Clock()
 while run:
     text = font.render("ARCADE", 1, white)
     window.blit(text, (310, 100))
-    pygame.display.update()
-
-    menu_buttons()
+    button1.draw()
+    #menu_buttons()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
+    clock.tick(60)
 pygame.quit()
 
