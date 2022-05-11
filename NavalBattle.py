def33c28a1c833e6bd87f1dbb66ff6519c070e9d
tobias <t.wendl28@gmail.com>
2022-05-11 09:59:45 +0200

    creation menu bataille navale

--- NavalBattle.py
+++ NavalBattle.py
@@ -10,12 +10,34 @@ def window_init():
     pygame.init()
     window = pygame.display.set_mode((800, 800))
     pygame.display.set_caption("THE NAVAL BATTLE")
+    bg_image = pygame.image.load('img/blue.jpg')
+    window.blit(bg_image, (0, 0))
+    return window
 
+def Button_creation():
+    button1 = Button('Back to menu', 200, 40, (400, 300), 5)
+    return button1
 
 def main_NavalBattle():
+
+    window = window_init()
+
+    #intialisation de la fenetre
     window_init()
+
+    #creation du boutton de retour au menu
+    button1 = Button_creation()
+
     run = True
     while run:
+        #affichage du boutton sur la fenetre
+        button1.draw(window)
+
+        if button1.pressed == True :
+            print("back to main menu")
+            button1.pressed = False
+            run = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
