20725b48ed116f09ab346f254443e919f77bcacb
tobias <t.wendl28@gmail.com>
2022-05-20 09:53:16 +0200

    ajout d'une input box et début affichage message

---
+++ InputBox.py
@@ -0,0 +1,51 @@
+#-*- coding: UTF-8 -*-
+import pygame
+from pygame import*
+
+inactive_color = (147, 173, 172)
+active_color = (205, 247, 245)
+font = pygame.font.Font(None, 32)
+
+class InputBox:
+
+    def __init__(self, text=''):
+        self.rect = pygame.Rect(180, 660, 400, 70)
+        self.color = inactive_color
+        self.text = text
+        self.txt_surface = font.render(text, True, self.color)
+        self.active = False
+        self.message = ''
+
+    def handle_event(self, event, window):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = active_color if self.active else inactive_color
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.message = self.text
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = font.render(self.text, True, self.color)
+        return self.message
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(400, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, window):
+        # Blit the text.
+        window.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(window, self.color, self.rect, 2)
--- chat.py
+++ chat.py
@@ -1,10 +1,11 @@
 #-*- coding: UTF-8 -*-
 
 import pygame
-import time
 from pygame import*
 import Button
 from Button import*
+import InputBox
+from InputBox import*
 
 def window_init():
     """initialisation de la fenêtre"""
@@ -22,7 +23,8 @@ def window_refresh(window):
 def button_creation():
     """creation des differents bouttons"""
     button1 = Button('Back to menu', 200, 40, (290, 670), 5)
-    return button1
+    button2 = Button('Send', 200, 40, (600, 680), 5)
+    return button1, button2
 
 def main_chat():
     """main du chat"""
@@ -30,20 +32,38 @@ def main_chat():
     window_init()
     clock = pygame.time.Clock()
 
-    button1 = button_creation()
+    button1, button2 = button_creation()
 
+    #colors
+    white = (255,255,255)
+
+    #input box
+    inputBox = InputBox()
+    boxlist = [inputBox]
     #booleans
     run = True
     while run:
-        button1.draw(window)
-        pygame.display.update()
+        #button1.draw(window)
+
+        pygame.draw.rect(window, white, (0, 0, 800, 650))
+        button2.draw(window)
+
 
         if button1.pressed == True:
             print('back to main menu')
             button1.pressed = False
             run = False
 
+
         #permet à l'utilisateur de quitter le jeu --> retour au menu principal
         for event in pygame.event.get():
+            for box in boxlist:
+                message = box.handle_event(event, window)
             if event.type == pygame.QUIT:
                 run = False
+        text = font.render(message, 1, (0, 0, 0))
+        window.blit(text, (0, 0))
+        pygame.display.update()
+        inputBox.update()
+        window_refresh(window)
+        inputBox.draw(window)
