319a2d526c93981a505ff5a31b3009ee715854b1
tobias <t.wendl28@gmail.com>
2022-06-05 19:16:50 +0200

    affichage impossible en dehors du cadre prévu

--- chat.py
+++ chat.py
@@ -25,8 +25,8 @@ def button_creation():
     """creation des differents bouttons"""
     button1 = Button('Back to menu', 200, 40, (290, 670), 5)
     button2 = Button('Send', 200, 40, (600, 680), 5)
-    button3 = Button('^', 20, 20, (100, 100), 5)
-    button4 = Button('v', 20, 20, (100, 130), 5)
+    button3 = Button('^', 20, 20, (100, 670), 5)
+    button4 = Button('v', 20, 20, (100, 700), 5)
     return button1, button2, button3, button4
 
 def message_content():
@@ -38,7 +38,7 @@ def message_content():
 
 def message_display(window, content, counter):
     """permet d'afficher les messages précédents dans le chat"""
-    xPos = 600
+    xPos = 550
     nbr_message = len(content)
 
     #on peut afficher maximum 12 messages sur l'écran
@@ -48,8 +48,20 @@ def message_display(window, content, counter):
         yPos = counter*50 - depassement*50
     else:
         yPos = 50
-    for i in range(len(content)):
-        text = font.render(content[i], 1, (0, 0, 0))
+
+    #on fait en sorte de ne pas afficher les messages au délà du cadre prévu
+    if counter > 1:
+        msgRange = len(content)-counter
+    else:
+        msgRange = len(content)
+
+    for i in range(msgRange):
+        #on supprime le caractère de retour chariot pour qu'il ne s'affiche pas dans pygame
+        msgLength = len(content[i])-1
+        message = ""
+        for j in range(msgLength):
+            message += content[i][j]
+        text = font.render("you : "+message, 1, (0, 0, 0))
         window.blit(text, (xPos, yPos))
         yPos += 50
 
@@ -106,7 +118,7 @@ def main_chat():
                 message = box.handle_event(event, window)
                 if message != '':
                     AllMessage = open('messages.txt', 'a')
-                    AllMessage.write('\n' + message)
+                    AllMessage.write(message + '\n')
                     box.message = ''
                     AllMessage.close()
 
