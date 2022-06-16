1667971c80f476fef0ebf1427b9a5eb78b36dce0
G3R74W <83832635+G3R74W@users.noreply.github.com>
2022-06-17 01:27:56 +0200

    Add files via upload

---
+++ IP_perso.py
@@ -0,0 +1,56 @@
+import socket
+import ping3 as ping
+
+
+
+def get_ip():
+    """
+    fonction qui permet d'avoir sa propore IP au sein d'un réseau
+    :return: revoie l'IP de l'ordinateur sur le reseau
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # on cree une socket vers une adresse qui n'a pas d' importance
+        # puis on obtient le premier element du nom de la socket
+        # cela nous donne notre IP
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def list_host (IP, min = 0, max = 255):
+    """
+    fonction qui permet d'avoir l'ensemble des ordinateurs connecte dans une plage voulu
+    :param IP: IP de l'utilisateur
+    :param min: borne inferieur de plage voulu
+    :param max: borne superieur de plage voulu
+    :return: liste contenant tous les appareil connecte dans la plage
+    """
+     # IP est une chaine sous la forme X1.X2.X3.X4
+    # on coupe la chaine sur les points
+    # puis on recupere les 3 premiers X pour avoir le reseau
+    # une fois qu'on a le reseau on creer une liste
+    #avec toutes possible les ip du reseau
+    reseau= ""
+    list_IP = IP.split(".")
+    i=0
+    for i in range(3):
+        reseau += list_IP[i] + "."
+    list_host = []
+    print("RESEAU = %s"%(reseau))
+    for j in range (min,max):
+        host = reseau + "%s"%(j)
+        list_host.append(host)
+    return list_host
+
+
+
+if __name__ == '__main__':
+    IP = get_ip()
+    print("mon IP est : %s"%(IP))
+    list_IP = list_host(IP,15,20)
+    print("la liste des hosts possible est : \n %s" %(list_IP))
